71d8a6863c42fda0cbe0626576e8d48dc8e8e24c
Miguel Rojas <miguel_rojas@mentor.com>
2017-07-31 17:14:51 -0400

    Refactored message in it's own module

--- js_source/states/play.js
+++ js_source/states/play.js
@@ -111,21 +111,3 @@ Game.playState = {
         }
     }
 }
-
-Game.Message = function() {
-    this.group = Engine.add.group();
-    this.group.fixedToCamera = true;
-}
-
-Game.Message.prototype.show = function(title, message) {
-    console.log("Game.Message: entering message mode")
-    Game.playState.gameStatus = Game.MESSAGE;
-    this.close();
-    this.group.add(Engine.add.text(10, 10, title, {font: "20px Arial", fill: "#ffffff"}));
-    this.group.add(Engine.add.text(10, 30, message, {font: "20px Arial", fill: "#ffffff"}));
-    this.group.add(Engine.add.text(10, 60, "<press a key>", {font: "20px Arial", fill: "#000000"}));
-}
-
-Game.Message.prototype.close = function() {
-    this.group.removeAll();
-}
