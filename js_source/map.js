781d92d717f3c54fb215b2049951bb12494d651f
Miguel Rojas <miguel_rojas@mentor.com>
2017-11-13 14:27:24 -0300

    Map loader

--- js_source/map.js
+++ js_source/map.js
@@ -5,9 +5,38 @@ var ZORPG = ZORPG || {};
  *  
  *  name: unique name of the map
  */
-ZORPG.Map = function(name, data) {
-    this.name = name;
-    this.data = [];
+ZORPG.Map = (function() {
+    var mapData = {
+        floor: [],
+        ceiling: [],
+        object: [],
+        walls: []
+    };
 
-    // Parse data
-};
+    return {
+        data: function() {
+            return mapData;
+        },
+
+        load: function(data) {
+            // Parse data
+            for (i = 0; i < data.layers.length; ++i) {
+                var layer = data.layers[i];
+
+                for (j = 0; j < layer.height; ++j) {
+                    var row = [];
+
+                    for (k = 0 + j * layer.width; k < (j + 1) * layer.width; ++k) {
+                        row.push(layer.data[k]);
+                    }
+
+                    mapData[layer.name].push(row);
+                }
+            }
+        },
+
+        about: function() {
+            return "About";
+        }
+    }
+})();
