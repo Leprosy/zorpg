413bbfbfa9d4386b638fcc09eed705170a1a41af
Miguel Rojas <miguel_rojas@mentor.com>
2018-01-09 16:06:12 -0300

    Building components into the lib.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # Dist build
 #node_modules/uglify-js/bin/uglifyjs js_source/boot.js js_source/states/* js_source/start.js js_source/lib/* -o js/game.js
 # Dev build
-node_modules/uglify-js/bin/uglifyjs js_source/*.js js_source/states/*.js --comments all -b -o js/game.js
+node_modules/uglify-js/bin/uglifyjs js_source/*.js js_source/states/*.js js_source/components/*.js --comments all -b -o js/game.js
 # node_modules/uglify-js/bin/uglifyjs js_source/boot.js js_source/start.js js_source/lib/* --comments all -b -o js/game.js
 #tsc -project ./ts_source/ -out ./js/game.js
