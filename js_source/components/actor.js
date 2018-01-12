5a35dcea5e6992fedd47366eacf1a7e1e9ffdf93
Miguel Rojas <miguel_rojas@mentor.com>
2018-01-12 16:24:54 -0300

    Debug attribute roll

--- js_source/components/actor.js
+++ js_source/components/actor.js
@@ -6,11 +6,21 @@ ZORPG.Components.actor = {
     xp: 0,
     level: 1,
     name: "",
-    speed: 0,
     str: 1,
     spd: 1,
     con: 1,
     ac: 0,
+
+    // Roll attributes - DEBUG only
+    roll: function() {
+        this.spd = ZORPG.Utils.die("1d10+4");
+        this.str = ZORPG.Utils.die("1d10+4");
+        this.con = ZORPG.Utils.die("1d10+4");
+        this.ac = ZORPG.Utils.die("1d10+4");
+
+        this.hp = ZORPG.Utils.die("1d30+10");
+        this.xp = ZORPG.Utils.die("1d50+10");
+    },
 
     toString: function() {
         return this.name + ":" + this.hp + "hp";
