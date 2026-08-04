9b42a3c1aa85a2ec047e1df58f2dacaa74aff718
Miguel Rojas <leprosy57@gmail.com>
2026-08-03 23:20:22 -0400

    Collisions!

--- screens/game.gd
+++ screens/game.gd
@@ -14,39 +14,42 @@ func debug() -> void:
     var mesh = grid.mesh_library.get_item_navigation_layers(cell_id)
     print(mesh)
 
+# Check if the move is valid using the navigation layers
+# The navigation layers are:
+# 1: Default
+# 2: Impassable
+func _check_move_valid(position: Vector3) -> bool:
+    var grid = $PC/VC/V/GridMap
+    var local_pos = grid.to_local(position)
+    var map_pos = grid.local_to_map(local_pos)
+    var cell_id = grid.get_cell_item(map_pos)
+    if cell_id == -1:
+        return false
+    var mesh = grid.mesh_library.get_item_navigation_layers(cell_id)
+    print(mesh)
+    return mesh == 1
 
+func _execute_move(direction: Vector3) -> void:
+    var new_position = $PC/VC/V/Party.global_position + direction * SPEED
+    if not self._check_move_valid(new_position):
+        return
+    var tween = get_tree().create_tween()
+    tween.tween_property($PC/VC/V/Party, "position", new_position , 0.15)
+    tween.play()
+    await tween.finished
+    self.debug()
 
 func _on_forward_pressed() -> void:
-    var tween = get_tree().create_tween()
-    var new_position = $PC/VC/V/Party.global_position - $PC/VC/V/Party.global_basis.z * SPEED
-    tween.tween_property($PC/VC/V/Party, "position", new_position , 0.15)
-    tween.play()
-    await tween.finished
-    self.debug()
+    self._execute_move(-$PC/VC/V/Party.global_basis.z)
 
 func _on_backwards_pressed() -> void:
-    var tween = get_tree().create_tween()
-    var new_position = $PC/VC/V/Party.global_position + $PC/VC/V/Party.global_basis.z * SPEED
-    tween.tween_property($PC/VC/V/Party, "position", new_position , 0.15)
-    tween.play()
-    await tween.finished    
-    self.debug()
+    self._execute_move($PC/VC/V/Party.global_basis.z)
 
 func _on_strafe_left_pressed() -> void:
-    var tween = get_tree().create_tween()
-    var new_position = $PC/VC/V/Party.global_position - $PC/VC/V/Party.global_basis.x * SPEED
-    tween.tween_property($PC/VC/V/Party, "position", new_position , 0.15)
-    tween.play()
-    await tween.finished
-    self.debug()
+    self._execute_move(-$PC/VC/V/Party.global_basis.x)
 
 func _on_strafe_right_pressed() -> void:
-    var tween = get_tree().create_tween()
-    var new_position = $PC/VC/V/Party.global_position + $PC/VC/V/Party.global_basis.x * SPEED
-    tween.tween_property($PC/VC/V/Party, "position", new_position , 0.15)
-    tween.play()
-    await tween.finished 
-    self.debug()
+    self._execute_move($PC/VC/V/Party.global_basis.x)
 
 func _on_turn_left_pressed() -> void:
     var tween = get_tree().create_tween()
