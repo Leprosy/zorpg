a7667407b73c5c558c21ea77edb2386bf23f43e9
Miguel Rojas <leprosy57@gmail.com>
2026-08-03 23:37:03 -0400

    Shake it baby

--- screens/game.gd
+++ screens/game.gd
@@ -1,18 +1,14 @@
 extends Screen
 
-# All geometries are 1m width/tall/depth
-const SPEED = 1
+const SPEED = 1 # All geometries are 1m width/tall/depth
+const CAMERA_SHAKE_BLOCKED_MOVE = 0.005
+const CAMERA_SHAKE_PERIOD = 0.15
+const MOVE_DURATION = 0.15
 
-func debug() -> void:
-    var grid = $PC/VC/V/GridMap
-    var local_pos = grid.to_local($PC/VC/V/Party.position)
-    print(local_pos)
-    var map_pos = grid.local_to_map(local_pos)
-    print(map_pos)
-    var cell_id = grid.get_cell_item(map_pos)
-    print(cell_id)
-    var mesh = grid.mesh_library.get_item_navigation_layers(cell_id)
-    print(mesh)
+enum NavigationLayers {
+    DEFAULT = 1,
+    IMPASSABLE = 2,
+}
 
 # Check if the move is valid using the navigation layers
 # The navigation layers are:
@@ -27,17 +23,30 @@ func _check_move_valid(position: Vector3) -> bool:
         return false
     var mesh = grid.mesh_library.get_item_navigation_layers(cell_id)
     print(mesh)
-    return mesh == 1
+    return mesh == NavigationLayers.DEFAULT
 
 func _execute_move(direction: Vector3) -> void:
     var new_position = $PC/VC/V/Party.global_position + direction * SPEED
     if not self._check_move_valid(new_position):
+        self._camera_shake(CAMERA_SHAKE_BLOCKED_MOVE)
         return
     var tween = get_tree().create_tween()
-    tween.tween_property($PC/VC/V/Party, "position", new_position , 0.15)
+    tween.tween_property($PC/VC/V/Party, "position", new_position , MOVE_DURATION)
     tween.play()
     await tween.finished
-    self.debug()
+
+func _camera_shake(magnitude: float) -> void:
+    var camera = $PC/VC/V/Party/Camera3D
+    var initial_transform: Transform3D = camera.transform # Store the full initial transform of the camera
+    var elapsed_time: float = 0.0
+    while elapsed_time < CAMERA_SHAKE_PERIOD:
+        var offset = Vector3(randf_range(-magnitude, magnitude),
+                            randf_range(-magnitude, magnitude),
+                            0.0)
+        camera.transform.origin = initial_transform.origin + offset
+        elapsed_time += get_process_delta_time()
+        await get_tree().process_frame
+    camera.transform = initial_transform # Reset back to the original transform
 
 func _on_forward_pressed() -> void:
     self._execute_move(-$PC/VC/V/Party.global_basis.z)
@@ -51,18 +60,15 @@ func _on_strafe_left_pressed() -> void:
 func _on_strafe_right_pressed() -> void:
     self._execute_move($PC/VC/V/Party.global_basis.x)
 
-func _on_turn_left_pressed() -> void:
+func _execute_turn(direction: int) -> void:
     var tween = get_tree().create_tween()
-    var new_position = $PC/VC/V/Party.rotation + Vector3(0, PI / 2, 0)
-    tween.tween_property($PC/VC/V/Party, "rotation", new_position , 0.15)
+    var new_position = $PC/VC/V/Party.rotation + Vector3(0, direction * PI / 2, 0)
+    tween.tween_property($PC/VC/V/Party, "rotation", new_position , MOVE_DURATION)
     tween.play()
     await tween.finished
-    self.debug()
+
+func _on_turn_left_pressed() -> void:
+    self._execute_turn(1)
 
 func _on_turn_right_pressed() -> void:
-    var tween = get_tree().create_tween()
-    var new_position = $PC/VC/V/Party.rotation - Vector3(0, PI / 2, 0)
-    tween.tween_property($PC/VC/V/Party, "rotation", new_position , 0.15)
-    tween.play()
-    await tween.finished
-    self.debug()
+    self._execute_turn(-1)
