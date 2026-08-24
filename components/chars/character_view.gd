c42e25b9aeaf2c4e9ce663575ecea939aebe161a
Miguel Rojas <leprosy57@gmail.com>
2026-08-23 23:28:34 -0400

    Tabs for char view

--- components/chars/character_view.gd
+++ components/chars/character_view.gd
@@ -1,4 +1,4 @@
-extends Panel
+extends TabContainer
 
 const MAX_ITEMS = 20
 
@@ -7,55 +7,52 @@ var selected_item_index: int = -1
 
 func set_data(data: Character) -> void:
     self.char_data = data
-    $ItemsView/ItemDetails.hide()
-
-    $Mgt/Value.text = "%s" % data.might
-    $End/Value.text = "%s" % data.endurance
-    $Spd/Value.text = "%s" % data.speed
-    $Int/Value.text = "%s" % data.intelligence
-    $Per/Value.text = "%s" % data.personality
-    $Name.text = "%s: %s %s" % [data.name, data.get_gender(), data.get_profession() ]
-    $ItemsView/ItemsName.text = "Items for %s the %s" % [data.name, data.get_profession()]
+    $Items/Details.hide()
+    self.render_stats()
     self.render_items()
 
+func render_stats() -> void:
+    var data = self.char_data
+    $Stats/Name.text = "%s: %s %s" % [data.name, data.get_gender(), data.get_profession() ]
+    $Stats/Mgt/Value.text = "%s" % data.might
+    $Stats/End/Value.text = "%s" % data.endurance
+    $Stats/Spd/Value.text = "%s" % data.speed
+    $Stats/Int/Value.text = "%s" % data.intelligence
+    $Stats/Per/Value.text = "%s" % data.personality
+
 func render_items() -> void:
+    var data = self.char_data
     var items = self.char_data.items
-    for child in $ItemsView/ItemsGrid.get_children():
+    $Items/Name.text = "Items for %s the %s" % [data.name, data.get_profession()]
+    for child in $Items/Grid.get_children():
        child.queue_free()
     var index = 0
     for item: Item in items:
         var comp = ItemComponent.create(item.type, item.equiped) as ItemComponent
-        comp.connect("gui_input", self.item_click.bind(item, index))
-        $ItemsView/ItemsGrid.add_child(comp)
+        comp.connect("gui_input", self._on_item_pressed.bind(item, index))
+        $Items/Grid.add_child(comp)
         if index == self.selected_item_index:
             comp.set_selected(true)
         index += 1
     for i in range(0, MAX_ITEMS - len(items)):
         var comp = ItemComponent.create(-1, false)
-        $ItemsView/ItemsGrid.add_child(comp)
+        $Items/Grid.add_child(comp)
 
-func _on_close_pressed() -> void:
-    self.visible = false
-
-func item_click(event: InputEvent, data: Item, index: int) -> void:
+func _on_item_pressed(event: InputEvent, data: Item, index: int) -> void:
     if event is InputEventMouseButton and not event.pressed:
         if self.selected_item_index >= 0:
-            $ItemsView/ItemsGrid.get_children()[self.selected_item_index].set_selected(false)
+            $Items/Grid.get_children()[self.selected_item_index].set_selected(false)
         self.selected_item_index = index
-        $ItemsView/ItemDetails.show()
-        $ItemsView/ItemDetails/Name.text = data.name
-        $ItemsView/ItemDetails/Icon.frame = data.type * 4
-        $ItemsView/ItemDetails/Data.text = "ac: %s\ndmg: %s" % [data.ac, data.dmg]
-        $ItemsView/ItemsGrid.get_children()[self.selected_item_index].set_selected(true)
-
-# TODO: Consider using tabs instead, if we want to implement another screen here
-func _on_items_pressed() -> void:
-    $ItemsView.show()
-
-func _on_items_view_close_pressed() -> void:
-    $ItemsView.hide()
+        $Items/Details.show()
+        $Items/Details/Name.text = data.name
+        $Items/Details/Icon.frame = data.type * 4
+        $Items/Details/Data.text = "ac: %s\ndmg: %s" % [data.ac, data.dmg]
+        $Items/Grid.get_children()[self.selected_item_index].set_selected(true)
 
 func _on_equip_pressed() -> void:
     var equiped = self.char_data.items[self.selected_item_index].equiped
     self.char_data.items[self.selected_item_index].equiped = not equiped
     self.render_items()
+
+func _on_close_pressed() -> void:
+    self.visible = false
